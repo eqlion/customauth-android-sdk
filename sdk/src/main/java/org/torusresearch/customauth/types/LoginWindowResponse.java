900df1d1de932fe2fd02b30e2532115b54ef8d63
Nikolay Lapa <lapanicholas@gmail.com>
2023-03-06 19:34:08 +0800

    fix .split error

--- sdk/src/main/java/org/torusresearch/customauth/types/LoginWindowResponse.java
+++ sdk/src/main/java/org/torusresearch/customauth/types/LoginWindowResponse.java
@@ -21,17 +21,17 @@ public class LoginWindowResponse {
     }
 
     public void parseResponse(String response) {
-        String[] params = response.substring(response.indexOf("#") + 1).split("&");
-        for (String param : params) {
-            String[] keyValuePair = param.split("=");
-            if (keyValuePair.length > 0) {
-                if (keyValuePair[0].equalsIgnoreCase("access_token")) {
-                    setAccessToken(keyValuePair[1]);
-                } else if (keyValuePair[0].equalsIgnoreCase("id_token")) {
-                    setIdToken(keyValuePair[1]);
-                }
-            }
-        }
+//        String[] params = response.substring(response.indexOf("#") + 1).split("&");
+//        for (String param : params) {
+//            String[] keyValuePair = param.split("=");
+//            if (keyValuePair.length > 0) {
+//                if (keyValuePair[0].equalsIgnoreCase("access_token")) {
+//                    setAccessToken(keyValuePair[1]);
+//                } else if (keyValuePair[0].equalsIgnoreCase("id_token")) {
+//                    setIdToken(keyValuePair[1]);
+//                }
+//            }
+//        }
     }
 
     @Override
